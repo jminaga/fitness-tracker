0b4714995cec693cfab163009644b2352718500b
Jared Minaga <jdm1code@gmail.com>
2021-09-30 21:33:43 -0600

    added index.html path in html routes

--- routes/html.js
+++ routes/html.js
@@ -8,4 +8,8 @@ module.exports = function (app) {
   app.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
+
+  app.get("/", function (req,res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  })
 };
